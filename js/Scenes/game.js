d9beafde5b13cb5c236dd5a02bb66941b3238d92
cmorenilla723 <u1980985@campus.udg.edu>
2023-06-01 16:13:51 +0200

    Update game.js

--- js/Scenes/game.js
+++ js/Scenes/game.js
@@ -13,7 +13,9 @@ class GameScene extends Phaser.Scene {
         this.l_partida=null;
         this.enemigosGroup= null;
         this.normalEnemys=['enemigo1','enemigo2','enemigo3'];
-        this.totalEnemys=0;
+        this.hardEnemys=['enemigo4','enemigo5'];
+        this.rareEnemys=['gnomo,bobEsponja'];
+        this.totalEnemys=10;
         this.puntos=0;
         this.textPoints=0;
         this.createEnemys=true;
@@ -23,9 +25,13 @@ class GameScene extends Phaser.Scene {
         this.load.image('boat','../images/Barco.jpg');
         this.load.image('background', '../images/PLACE_HOLDER_FONDO.jpg');
         this.load.image('canya', '../images/canyaPescar.png');
-        this.load.image('enemigo1', '../images/enemy_1.png');
-        this.load.image('enemigo2', '../images/enemy_2.png');
-        this.load.image('enemigo3', '../images/enemy_3.png');
+        this.load.image('enemigo1', '../images/peix1.png');
+        this.load.image('enemigo2', '../images/peix2.png');
+        this.load.image('enemigo3', '../images/peix3.png');
+        this.load.image('enemigo4', '../images/peix4.png');
+        this.load.image('enemigo5', '../images/peix5.png');
+        this.load.image('bobEsponja', '../images/peixBobEsponja.png');
+        this.load.image('gnomo', '../images/peixGnomo.png');
 
     }
     create (){
@@ -47,7 +53,6 @@ class GameScene extends Phaser.Scene {
         }
         const firstImageWidth=370;
         const firstImageHeight=100;
-        
         { //Create background image
             const image = this.add.image(firstImageWidth, firstImageHeight, 'boat');  // Colocar la imagen en la esquina superior izquierda
             image.setScale(0.5);
@@ -75,33 +80,63 @@ class GameScene extends Phaser.Scene {
 
         { //creacion enemigos y fisicas entre ellos.
             this.enemigosGroup = this.physics.add.group();
-            this.crearEnemigos(this.normalEnemys, this.totalEnemys,Y1,Y2);
+            this.crearEnemigos(this.normalEnemys, this.totalEnemys,Y1,Y2,0.5);
             this.agregarColisiones();
         }
 
     }
+    //se agregan colisiones a todos los sprites nuevos y viejos que se generan.
     agregarColisiones() {
         this.physics.add.overlap(this.player, this.enemigosGroup, this.handleCollision, null, this);
     }
     
-    crearEnemigos(tiposEnemigos, totalEnemigos,Y1,Y2){
+    //se crean los enemigos
+    crearEnemigos(tiposEnemigos, totalEnemigos,Y1,Y2,escalado){
         for (let i = 0; i < totalEnemigos; i++) {
             const posX = Phaser.Math.RND.between(30, this.scale.width-20);
             const posY = Phaser.Math.RND.between(Y1, Y2);
 
             const tipoEnemigo = Phaser.Math.RND.pick(tiposEnemigos);
-
+            const puntuacio= this.comprobarEnemigo(tipoEnemigo);
             const enemigo = {
-                sprite: this.physics.add.sprite(posX, posY, tipoEnemigo).setScale(0.1),
-                puntos: Phaser.Math.RND.integerInRange(1, 25),
+                sprite: this.physics.add.sprite(posX, posY, tipoEnemigo).setScale(escalado),
+                puntos: puntuacio,
                 velocidad: Phaser.Math.RND.between(1, 3), // Velocidad aleatoria
                 direccion: 1, // Dirección inicial hacia la derecha (1) o izquierda (-1)
                 tipo: tipoEnemigo
             };
             enemigo.sprite.setData('enemigoData', enemigo); // Guardar enemigo como dato personalizado
             this.enemigosGroup.add(enemigo.sprite);
-
         }
+    }
+
+    //para no tenerlo todo en una sola funcion se llama a este metodo que devuelve la puntuacion en base al tipo de enemigo que le pasamos.
+    comprobarEnemigo(tipoEnemigo) {
+        let puntuacio=0;
+        switch (tipoEnemigo) {
+            case 'enemigo1':
+                puntuacio=4;
+                break;
+            case 'enemigo2':
+                puntuacio=5;
+                break;
+            case 'enemigo3':
+                puntuacio=8;
+                break;
+            case 'enemigo4':
+                puntuacio=10;
+                break;
+            case 'enemigo5':
+                puntuacio=15;
+                break;
+            case 'gnomo':
+                puntuacio=22;
+                break;
+            case 'bobEsponja':
+                puntuacio=22;
+                break;
+        }
+        return puntuacio;
     }
 
     update (){
@@ -121,12 +156,22 @@ class GameScene extends Phaser.Scene {
         this.text.y=this.camera.scrollY;
         this.textPoints.y=this.camera.scrollY;
         this.comprobar();
-        if((this.player.y>400 && this.player.y<500)&& this.createEnemys){
-            this.crearEnemigos(this.normalEnemys,this.totalEnemys,400,500);
+
+        if((this.player.y>650 && this.player.y<1000)&& this.createEnemys){
+            this.crearEnemigos(this.normalEnemys,this.totalEnemys,400,500,0.5);
             this.createEnemys=false;
         }
+        /*else if((this.player.y>1100 && this.player.y<1500)&& !this.createEnemys){
+            this.totalEnemys=13;
+            this.crearEnemigos(this.hardEnemys,this.totalEnemys,400,500,0.5);
+            this.totalEnemys=2; this.crearEnemigos(this.hardEnemys,this.totalEnemys,1100,1500,0.5);
+            this.createEnemys=true;
+        }*/
+
         console.log(this.player.y);
     }
+    
+    //comprobar los metros en el que estamos mediante la posicion del player
     comprobar(){
         if (this.speed > 0 && this.player.y - this.lastIncrementPosition >= 10) {
             // Cada vez que la cámara se desplace 10 píxeles hacia abajo, aumentamos los metros
@@ -144,6 +189,7 @@ class GameScene extends Phaser.Scene {
         //console.log('Player y:'," ", this.player.y," ", 'Metros:', this.metros," ", this.speed);
     }
 
+    //comportamiento de los peces
     movimientoEnemigos() {
         //por cada iteracion del forEach, calculamos en cada uno en funcion de su velocidad, la direccion donde irá y si toca las paredes, retorna en
         //sentido contrario
@@ -159,6 +205,7 @@ class GameScene extends Phaser.Scene {
         });
     }
 
+    //tratar la colision y llamar a otra funcion que se encarga de aumentar la puntuacion y eliminar el "enemy"
     handleCollision(player, enemySprite) {
         // Obtener el tipo de enemigo colisionado
         const enemy = this.enemigosGroup.getChildren().find(enemigo => enemigo === enemySprite);
